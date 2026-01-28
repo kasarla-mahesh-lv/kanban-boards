24d3c42eafce03d0fa7dacd75414536b2889920f
Lokesh Reddy <lokeshreddy@gmail.com>
2026-01-28 16:33:56 +0530

    new save

---
+++ front-end/src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+  return (
+    <header className='h-16 bg-white shadow flex items-center px-6'>
+        <h2 className='text-xl font-semibold'>Dashboard</h2>
+    </header>
+  )
+}
