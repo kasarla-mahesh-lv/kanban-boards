f6a0e549de378780fb52ff892fba32b130d13b16
Sravanireddy31 <sravanireddygorre31@gmail.com>
2026-01-29 18:35:22 +0530

    new one

--- back-end/src/models/board.js
+++ back-end/src/models/board.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const boardSchema = new mongoose.Schema(
-  { name: { type: String, required: true, trim: true } },
+  { name: { type: String, required: true } },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Board", boardSchema);
+module.exports = mongoose.models.Board || mongoose.model("Board", boardSchema);
