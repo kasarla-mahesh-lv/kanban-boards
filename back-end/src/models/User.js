3d66294ac8e6668785b3142a16193317c5b55ab9
Sowmya <rachurisowmya6@gmail.com>
2026-01-31 15:53:13 +0530

    conflicts8

--- back-end/src/models/User.js
+++ back-end/src/models/User.js
@@ -32,12 +32,12 @@ const userSchema=new mongoose.Schema(
     {timestamps:true}
 );
 
-userSchema.pre("save", async function (next) {
+/*userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
-});
+});*/
 
 module.exports=mongoose.model("User",userSchema);
