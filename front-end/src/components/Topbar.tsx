13843f527c612bfe96fdefbeed30420b2eb0e911
chandinireddymasu <reddymasuchandu@gmail.com>
2026-01-31 11:17:30 +0530

    updated

--- front-end/src/components/Topbar.tsx
+++ front-end/src/components/Topbar.tsx
@@ -1,28 +1,21 @@
-import './Topbar.css';
+import "./Topbar.css";
 
 export const Topbar = () => {
   return (
-    <div className="container-fluid mt-3 topbar">
-      <div className="d-flex align-items-center justify-content-between">
+    <div className="topbar">
+      <input
+        type="text"
+        className="topbar-search"
+        placeholder="Type to search"
+      />
 
-
-        <input
-          type="text"
-          className="form-control topbar-search"
-          placeholder="Type to search"
-        />
-
-        
-        <div className="d-flex align-items-center gap-3">
-          <div className="topbar-icon">
-            <i className="bi bi-sun-fill"></i>
-          </div>
-          <div className="topbar-icon">
-            <i className="bi bi-cloud"></i>
-            
-          </div>
+      <div className="topbar-actions">
+        <div className="topbar-icon">
+          <i className="bi bi-sun-fill"></i>
         </div>
-
+        <div className="topbar-icon">
+          <i className="bi bi-cloud"></i>
+        </div>
       </div>
     </div>
   );
