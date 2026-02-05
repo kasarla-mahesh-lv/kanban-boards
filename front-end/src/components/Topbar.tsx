2a36ab36754964954a9003912701d6c32d43ea0a
Lokesh Reddy <lokeshreddy@gmail.com>
2026-02-05 12:40:01 +0530

    topbar file change

--- front-end/src/components/Topbar.tsx
+++ front-end/src/components/Topbar.tsx
@@ -1,28 +1,20 @@
 import "./Topbar.css";
+export const Topbar=()=>{
+  return(
+    <div className="topbar">
+      <input type="text" className="topbar-search" placeholder="Type to search"/>
+      <div className="topbar-actions">
+        <div className="topbar-icons">
+          <i className="bi bi-sun-fill"></i>
 
-export const Topbar = () => {
-  return (
-    <div className="container-fluid mt-3 topbar">
-      <div className="d-flex align-items-center justify-content-around">
-
-
-        <input
-          type="text"
-          className="form-control topbar-search"
-          placeholder="Type to search"
-        />
-
-        
-        <div className="d-flex align-items-center gap-3">
-          <div className="topbar-icon">
-            <i className="bi bi-sun-fill"></i>
-          </div>
-          <div className="topbar-icon">
-            <i className="bi bi-cloud"></i>
-
-          </div>
         </div>
+        <div className="topbar-icon">
+          <i className="bi bi-cloud"></i>
+
+        </div>
+
       </div>
+
     </div>
-  );
-};
+  )
+}
