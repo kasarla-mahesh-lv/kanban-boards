2c7e14abc04c1520ee18e5832d32b157854036f5
ajayvemala02 <ajayvemala02@zohomail.in>
2026-02-09 18:21:08 +0530

    added env api url

--- front-end/src/components/Api/ApiService.tsx
+++ front-end/src/components/Api/ApiService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* ======================= AXIOS INSTANCE ======================= */
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change if needed
+  baseURL: import.meta.env.VITE_API_BASE_URL, // change if needed
   headers: {
     "Content-Type": "application/json",
   },
