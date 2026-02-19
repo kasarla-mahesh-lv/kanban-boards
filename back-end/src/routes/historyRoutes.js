881239d677dfb35bb2adb9a06a9feb002bb3cf47
Muppaneni Chaitanya <chaitu.chowdary2004@gmail.com>
2026-02-19 11:16:39 +0530

    Updated History

--- back-end/src/routes/historyRoutes.js
+++ back-end/src/routes/historyRoutes.js
@@ -13,5 +13,51 @@ const authMiddleware = require("../middlewares/authmiddlewares");
  *         description: Full history fetched successfully
  */
 router.get("/",authMiddleware,historyController.getAllHistory);
+/**
+ * @swagger
+ * /api/history/{taskId}:
+ *   get:
+ *     summary: Get history for a specific task
+ *     tags: [History]
+ *     parameters:
+ *       - in: path
+ *         name: taskId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Task ID
+ *     responses:
+ *       200:
+ *         description: Task history fetched successfully
+ */
+router.get("/:taskId", authMiddleware, historyController.getTaskHistory);
+/**
+ * @swagger
+ * /api/history:
+ *   post:
+ *     summary: Create new history record
+ *     tags: [History]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - taskId
+ *               - activity
+ *             properties:
+ *               taskId:
+ *                 type: string
+ *               activity:
+ *                 type: string
+ *               createdBy:
+ *                type: string
+ *     responses:
+ *       201:
+ *         description: History created successfully
+ */
+
+router.post("/", authMiddleware, historyController.createHistory);
 
 module.exports = router;
